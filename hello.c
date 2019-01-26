85752b49d15845f803f81d13957831a943b0115c
ATruglio <andreat72001@gmail.com>
2019-01-26 12:25:55 +0000

    Test C - Hello World!

---
+++ hello.c
@@ -0,0 +1,9 @@
+/* Test C - Hello World! */
+
+#include <stdio.h>
+void main ()
+{
+	printf("Hello World!\n"
+	"My name's Andrea Truglio.\n\n"
+	"I study ICT at \"Carlo Alberto dalla Chiesa\" Technical Institute in Afragola, near Naples, in the southern Italy.\n");
+}
